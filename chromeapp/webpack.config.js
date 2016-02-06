4ed57e5edc42056e28adf8829c2c173bcfa24e24
kunalb <kunal.bhagawati@gmail.com>
2016-02-06 23:52:19 +0530

    Fix copy bug

--- chromeapp/webpack.config.js
+++ chromeapp/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
 
     plugins: [
         new CopyWebpackPlugin([
-            {from: './manifest.json', to: 'build/'}
+            {from: '../manifest.json', to: '../manifest.json'}
         ])
     ]
 };
