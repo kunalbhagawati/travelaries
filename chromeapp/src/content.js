f96fdba9f6c1eba8e86fd3de4f8e006cc1a418a9
kunalb <kunal.bhagawati@gmail.com>
2016-02-06 23:02:47 +0530

    Add webpack

---
+++ chromeapp/src/content.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kunal on 6/2/16.
+ */
---
+++ chromeapp/webpack.config.js
@@ -0,0 +1,36 @@
+path = require('path');
+
+
+module.exports = {
+    context: path.join(__dirname, "src"),
+
+    entry: {
+        background: './background.js',
+        content: './content.js'
+    },
+
+    output: {
+        path: path.join(__dirname, 'build/js'),
+        filename: '[name]-bundle.js'
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        },
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/,
+                loader: 'file'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css!less"
+            }]
+    }
+};
