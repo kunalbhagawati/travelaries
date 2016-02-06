93a8b1bfdc44a127b4aa39cd528b541c232ff02d
kunalb <kunal.bhagawati@gmail.com>
2016-02-06 20:52:48 +0530

    Add rest framework. Add package file for chrome extension

--- datalayer/datalayer/datalayer/settings.py
+++ datalayer/datalayer/datalayer/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 
@@ -126,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+
+}
